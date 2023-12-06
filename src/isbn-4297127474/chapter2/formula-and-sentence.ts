491ae5b1e2a871ba69783f194091943c0a435eda
sakuyacatcat <shingo.takagi0116@gmail.com>
2023-12-07 01:19:48 +0900

    formula, sentence, sentence-formula

---
+++ src/isbn-4297127474/chapter2/formula-and-sentence.ts
@@ -0,0 +1,13 @@
+const greeting1: string = "Hello ";
+const target1: string = "World";
+console.log(greeting1 + target1);
+
+const greeting2: string = "Hello ";
+const target2: string = "World";
+const text2 = greeting2 + target2;
+console.log(text2);
+
+const greeting3 = "Hello, ",
+  target3 = "World",
+  text3 = greeting3 + target3;
+console.log(text3);
