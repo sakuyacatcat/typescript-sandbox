4f98c8c33a623032b39b34a786b4b7d590fab710
sakuyacatcat <shingo.takagi0116@gmail.com>
2023-12-28 11:45:50 +0900

    list

--- src/isbn-4297127474/chapter3/list.ts
+++ src/isbn-4297127474/chapter3/list.ts
@@ -1,7 +1,27 @@
-const arr: Array<number | string> = [1, "10", 100];
+// basic
+
+const arr: Array<number | string> = [1, "10", 100, "1000"];
 
 arr.push("arr");
 
+console.log("--- print arr element ---");
 for (const elm of arr) {
   console.log(elm);
 }
+console.log("--- print arr element ---");
+
+// split assign
+
+const [first, second, , forth] = arr;
+
+console.log("--- print split assign result from arr include four elm ---");
+console.log(first, second, forth);
+console.log("--- print split assign result from arr include four elm ---");
+
+// split assign with rest pattern
+
+const [...restList] = arr;
+
+console.log("--- print rest elm ---");
+console.log(restList);
+console.log("--- print rest elm ---");
