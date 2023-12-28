ec1d2943f5af3b86afdda16835df6618df994348
sakuyacatcat <shingo.takagi0116@gmail.com>
2023-12-28 12:51:56 +0900

    chapter3 training

---
+++ src/isbn-4297127474/chapter3/training/user-data.ts
@@ -0,0 +1,41 @@
+type User = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+};
+
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
+`;
+
+const users: User[] = [];
+const lines: string[] = data.split("\n");
+
+for (const line of lines) {
+  if (line === "") {
+    continue;
+  }
+
+  const [n, aString, pString] = line.split(",");
+  const name: string = String(n);
+  const age: number = Number(aString);
+  const premiumUser: boolean = Number(pString) === 1;
+  const user: User = {
+    name,
+    age,
+    premiumUser,
+  };
+  users.push(user);
+}
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name} ${user.age} はプレミアムユーザーです。`);
+  } else {
+    console.log(
+      `${user.name} ${user.age} はプレミアムユーザーではありません。`
+    );
+  }
+}
