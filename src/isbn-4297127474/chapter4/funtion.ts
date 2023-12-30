fa0a06ce7af0b2b6f12bdd3c4fb1cb9d3727e286
sakuyacatcat <shingo.takagi0116@gmail.com>
2023-12-30 13:38:16 +0900

    variable length arguments functions

--- src/isbn-4297127474/chapter4/funtion.ts
+++ src/isbn-4297127474/chapter4/funtion.ts
@@ -51,3 +51,48 @@ const total = (...args: number[]): number => {
 };
 
 console.log(total(1, 2, 3, 4, 5));
+
+const total2 = (base: number, ...args: number[]): number => {
+  let result = base;
+  for (const num of args) {
+    result += num;
+  }
+  return result;
+};
+
+console.log(total2(1, 2, 3, 4, 5));
+
+const totalArgs = [1, 2, 3, 4, 5];
+console.log(total(...totalArgs, 6, ...totalArgs));
+
+// function with optional arguments
+
+const toLowerOrUpper = (str: string, upper?: boolean): string => {
+  if (upper) {
+    console.log(upper);
+    return str.toUpperCase();
+  } else {
+    console.log(upper);
+    return str.toLowerCase();
+  }
+};
+
+console.log(toLowerOrUpper("Hello"));
+console.log(toLowerOrUpper("Hello", true));
+console.log(toLowerOrUpper("Hello", false));
+console.log(toLowerOrUpper("Hello", undefined));
+
+const toLowerOrUpper2 = (str: string, upper: boolean = false): string => {
+  if (upper) {
+    console.log(upper);
+    return str.toUpperCase();
+  } else {
+    console.log(upper);
+    return str.toLowerCase();
+  }
+};
+
+console.log(toLowerOrUpper2("Hello"));
+console.log(toLowerOrUpper2("Hello", true));
+console.log(toLowerOrUpper2("Hello", false));
+console.log(toLowerOrUpper2("Hello", undefined));
