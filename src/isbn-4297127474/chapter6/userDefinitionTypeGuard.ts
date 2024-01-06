df777c236e19e3702a72cccbf9e550a868f3bae9
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-01-07 02:10:21 +0900

    user define type filtering method without any type

--- src/isbn-4297127474/chapter6/userDefinitionTypeGuard.ts
+++ src/isbn-4297127474/chapter6/userDefinitionTypeGuard.ts
@@ -47,3 +47,26 @@ function checkAndUseHuman(value: unknown) {
   const name = value.name;
   return name;
 }
+
+// writing style without any
+
+function isPropertyAccessible(
+  value: unknown
+): value is { [key: string]: unknown } {
+  return value != null;
+}
+
+function isHuman2(value: unknown): value is Human9 {
+  if (!isPropertyAccessible(value)) return false;
+  console.log(value.type);
+  console.log(typeof value.name);
+  console.log(typeof value.age);
+
+  return (
+    value.type === "human" &&
+    typeof value.name === "string" &&
+    typeof value.age === "number"
+  );
+}
+
+console.log(isHuman2("test"));
