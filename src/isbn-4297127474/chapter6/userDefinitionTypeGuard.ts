77967427f5a65f3fc7309c0f3f9b74ea90929e03
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-01-07 01:36:12 +0900

    user definition type

--- src/isbn-4297127474/chapter6/userDefinitionTypeGuard.ts
+++ src/isbn-4297127474/chapter6/userDefinitionTypeGuard.ts
@@ -8,3 +8,42 @@ const something: unknown = 123;
 if (isStringOrNumber(something)) {
   console.log(something.toString());
 }
+
+// complex example
+
+type Human9 = {
+  type: "human";
+  name: string;
+  age: number;
+};
+
+function isHuman(value: any): value is Human9 {
+  if (value === null || value === undefined) return false;
+  return (
+    value.type === "human" &&
+    typeof value.name === "string" &&
+    typeof value.age === "number"
+  );
+}
+
+// assert syntax
+
+function assertHuman(value: any): asserts value is Human9 {
+  if (value == null) {
+    throw new Error("Given value is null or undefined");
+  }
+
+  if (
+    value.type !== "human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error("Given value is not a value of Human9 type");
+  }
+}
+
+function checkAndUseHuman(value: unknown) {
+  assertHuman(value);
+  const name = value.name;
+  return name;
+}
